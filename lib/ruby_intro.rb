b4ab41fa498819bf512695989d0146b0e2bcb0ac
noceo <paulschade20@gmail.com>
2022-02-07 22:49:49 -0600

    feat: finished part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,17 +1,40 @@
-# When done, submit this entire file to the autograder.
-
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0) { |sum, num| sum + num }
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    max_values = arr.max(2)
+    return max_values[0] + max_values[1]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  sorted_arr = arr.sort
+  start_pointer = 0
+  end_pointer = arr.length - 1
+
+  if arr.empty?
+    return false
+  end
+
+  while start_pointer != end_pointer
+    if sorted_arr[start_pointer] + sorted_arr[end_pointer] > n
+      end_pointer -= 1
+    elsif sorted_arr[start_pointer] + sorted_arr[end_pointer] < n
+      start_pointer += 1
+    else
+      return true
+    end
+  end
+
+  return false
 end
 
 # Part 2
